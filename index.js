cbdac2fdd5ebd3fc3189154ca13c37fa38f13959
Kitty Jacobs <kitty.jacobs@immalle.eu>
2018-01-06 19:29:15 +0100

    Add controllers and routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const engines = require("consolidate");
 
+const HomeRouter = require("./routes/home.server.route");
+
 const app = express();
 
 // Translates URLs to JSON
@@ -13,3 +15,15 @@ app.use(bodyParser.urlencoded());
 // Used for css, js and html
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "")));
+
+// Sets the render engine for front end pages to html
+// There are other replacements for html such pug or ejs
+app.engine("/html", engines.mustache);
+app.set("view engine", "html");
+app.engine("html", require("ejs").renderFile);
+
+app.use("/", HomeRouter);
+
+app.listen(8080, function(){
+    console.log("Stellaris ethnics quiz listening on port 8080!");
+})
